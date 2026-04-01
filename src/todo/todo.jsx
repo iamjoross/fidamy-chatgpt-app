8c720a47916cea5cb4a2a539851cffa11a28b89c
Jose Barredo <jose.barredo@clairhelpt.nl>
2026-04-01 13:53:15 +0200

    feat: bootstrap ui project

---
+++ src/todo/todo.jsx
@@ -0,0 +1,10 @@
+/* ================================ App =================================== */
+export function App() {
+  return <div className="my-5 antialiased">Hello</div>;
+}
+
+/* ---------- Mount helper for local testing ---------- */
+// const root = createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+export default App;
